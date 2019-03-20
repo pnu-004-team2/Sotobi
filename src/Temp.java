2ada745007316aa240d4012a85abec7cdf09a96d
KimGyuTae <KimGyuTae@DESKTOP-LS0G2OL>
2019-03-20 18:59:23 +0900

    #SAQI-2

--- src/Temp.java
+++ src/Temp.java
@@ -3,6 +3,7 @@ public class Temp {
 	public static void main(String[] args)  {
 		
 		System.out.println("Hot");
+		System.out.println("Hot");
 
 		System.out.println("ppp");
 		System.out.println("kkk");
